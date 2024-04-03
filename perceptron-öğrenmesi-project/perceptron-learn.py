6dcfbd40fbb3e1349fc39fe32f80123c54d29dd3
FIRAT KAAN BİTMEZ <74864221+firatkaanbitmez@users.noreply.github.com>
2024-04-04 01:02:57 +0300

    add

--- perceptron-öğrenmesi-project/perceptron-learn.py
+++ perceptron-öğrenmesi-project/perceptron-learn.py
@@ -2,16 +2,14 @@ import numpy as np
 
 class Perceptron:
     def __init__(self, num_inputs, agirliklar=None, esik_degeri_sabit=None, ogrenme_orani=0.5, epoch_sayisi=6):
-        """
-        Perceptron sınıfının başlatıcı metodu.
         
-        Args:
-        - num_inputs (int): Giriş özelliklerinin sayısı.
-        - agirliklar (numpy.ndarray, optional): Ağırlık vektörü. Varsayılan olarak rastgele oluşturulur.
-        - esik_degeri_sabit (float, optional): Eşik değeri sabiti. Varsayılan olarak rastgele oluşturulur.
-        - ogrenme_orani (float, optional): Öğrenme oranı. Varsayılan değeri 0.5'tir.
-        - epoch_sayisi (int, optional): Eğitim döngüsü sayısı. Varsayılan olarak 6'dır.
-        """
+
+        #num_inputs (int): Giriş özelliklerinin sayısı.
+        #agirliklar (numpy.ndarray, optional): Ağırlık vektörü. Varsayılan olarak rastgele oluşturulur.
+        #esik_degeri_sabit (float, optional): Eşik değeri sabiti. Varsayılan olarak rastgele oluşturulur.
+        #ogrenme_orani (float, optional): Öğrenme oranı. Varsayılan değeri 0.5'tir.
+        #epoch_sayisi (int, optional): Eğitim döngüsü sayısı. Varsayılan olarak 6'dır.
+       
         self.num_inputs = num_inputs
         if agirliklar is None:
             self.agirliklar = np.random.rand(num_inputs)
@@ -27,38 +25,18 @@ class Perceptron:
         self.epoch_sayisi = epoch_sayisi
 
     def aktivasyon(self, toplam):
-        """
-        Aktivasyon fonksiyonu.
         
-        Args:
-        - toplam (float): Girişlerle ağırlıkların nokta çarpımının toplamı.
+        #toplam (float): Girişlerle ağırlıkların nokta çarpımının toplamı.
         
-        Returns:
-        - int: Aktivasyon sonucu.
-        """
         return 1 if toplam > self.esik_degeri_sabit else 0
 
     def tahmin(self, girisler):
-        """
-        Perceptron'un tahmin yapma işlemi.
-        
-        Args:
-        - girisler (numpy.ndarray): Giriş özellikleri.
-        
-        Returns:
-        - int: Tahmin sonucu.
-        """
+      
         toplam = np.dot(girisler, self.agirliklar) + self.esik_degeri_sabit
         return self.aktivasyon(toplam)
 
     def egit(self, egitim_girisleri, etiketler):
-        """
-        Perceptron'u eğiten metot.
-        
-        Args:
-        - egitim_girisleri (numpy.ndarray): Eğitim veri seti giriş özellikleri.
-        - etiketler (numpy.ndarray): Eğitim veri seti etiketleri.
-        """
+       
         print("Eğitim başladı...")
         for epoch in range(self.epoch_sayisi):
             print(f"Epoch {epoch + 1}")
@@ -94,13 +72,13 @@ for i in range(len(egitim_girisleri[0])):
         agirlik_str = input(f"Ağırlık {i+1} (örn: 0.1): ").strip()
     agirliklar.extend(map(float, agirlik_str.split(',')))
 
-esik_degeri_sabit_str = input("Eşik değeri (örn: 0.5): ").strip()
+esik_degeri_sabit_str = input("Eşik değeri (örn: 1): ").strip()
 while not esik_degeri_sabit_str or not esik_degeri_sabit_str.replace('.', '', 1).isdigit():
     print("Geçerli bir eşik değeri girmediniz. Lütfen geçerli bir sayı girin.")
     esik_degeri_sabit_str = input("Eşik değeri (örn: 1): ").strip()
 esik_degeri_sabit = float(esik_degeri_sabit_str)
 
-ogrenme_orani_str = input("Öğrenme Oranı (örn: 0.1): ").strip()
+ogrenme_orani_str = input("Öğrenme Oranı (örn: 0.42): ").strip()
 while not ogrenme_orani_str or not all(x.replace('.', '', 1).isdigit() for x in ogrenme_orani_str.split(',')):
     print("Geçerli bir öğrenme oranı girmediniz. Lütfen geçerli bir sayı girin.")
     ogrenme_orani_str = input("Öğrenme Oranı (örn: 0.42): ").strip()
